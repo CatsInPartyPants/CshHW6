8dc30f271bfe99d6faa9f63351340163a3ab574b
Artem Nilov <nilov1235@mail.ru>
2023-01-20 17:03:35 +0300

    1,2 complete

--- CshHW6/Program.cs
+++ CshHW6/Program.cs
@@ -17,7 +17,7 @@ namespace CshHW6
             journal1.ShowCountOfWorkers();
             journal1 += 10;
             journal1.ShowCountOfWorkers();
-            Console.ReadKey();
+            //Console.ReadKey();
 
             Journal journal2 = new Journal("Москва");
             journal2.SetCountOfWorkers(54);
@@ -31,7 +31,23 @@ namespace CshHW6
                 Console.WriteLine($"Число работников в журнале {journal1.ToString()} больше чем в {journal2.ToString()}");
             }
 
+           // Console.ReadKey();
+
+            //task 2
+
+            Shop sh1 = new Shop("Магнит");
+            Shop sh2 = new Shop("Пятерочка");
+
+            sh1.SetArea(512.1);
+            sh2.SetArea(614.3);
+            sh1.ShowArea();
+            sh2.ShowArea();
+            sh1 += 50;
+            sh2 -= 144.3;
+            sh1.ShowArea();
+            sh2.ShowArea();
             Console.ReadKey();
+
 
         }
     }
---
+++ CshHW6/Shop.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CshHW6
+{
+    internal class Shop
+    {
+        private string _name;
+        private string _adress;
+        private string _discription;
+        private string _phone;
+        private string _email;
+        private double _area;
+
+        public Shop(string name)
+        {
+            _name = name;
+        }
+
+        public void ChangeName(string name)
+        {
+            _name = name;
+        }
+
+        public void SetAdress(string adr)
+        {
+            _adress = adr;
+        }
+
+        public void SetDiscription(string discr)
+        {
+            _discription = discr;
+        }
+
+        public void SetPhone(string ph)
+        {
+            _phone = ph;
+        }
+
+        public void SetEmail(string email)
+        {
+            _email = email;
+        }
+
+
+        public void ShowShopInfo()
+        {
+            Console.WriteLine($"Информация о магазине {_name}");
+            Console.WriteLine($"Магазин расположен по адресу {_adress}");
+            Console.WriteLine($"Для связи с руководством используйте телефон {_phone} " +
+                $"или email {_email}");
+            Console.WriteLine($"Описание магазина: {_discription}");
+        }
+
+        //task 2
+        public void SetArea(double area)
+        {
+            _area = area;
+        }
+
+        public void ShowArea()
+        {
+            Console.WriteLine($"The area of the shop {_name} is {_area}");
+        }
+
+        public static Shop operator +(Shop sh, double a)
+        {
+            sh._area += a;
+            return sh;
+        }
+
+        public static Shop operator -(Shop sh, double a)
+        {
+            sh._area -= a;
+            return sh;
+        }
+
+        public static bool operator ==(Shop sh1, Shop sh2)
+        {
+            return sh1._area == sh2._area;
+        }
+
+        public static bool operator !=(Shop sh1, Shop sh2)
+        {
+            return !(sh1._area == sh2._area);
+        }
+
+        public static bool operator <(Shop sh1, Shop sh2)
+        {
+            return (sh1._area < sh2._area);
+        }
+
+        public static bool operator >(Shop sh1, Shop sh2)
+        {
+            return (sh1._area > sh2._area);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Shop)) return false;
+            return this == (Shop)obj;
+        }
+
+
+
+    }
+}
